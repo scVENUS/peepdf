51dd6a790c4fabdf55e586fd0974d1c3a6405a62
Angelo Dell'Aera <buffer@olografix.org>
2016-11-25 09:30:05 +0100

    Updated requirements

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ setup(
     license="GNU GPLv3",
     url="http://eternal-todo.com",
     install_requires=[
-        "jsbeautifier==1.6.2",
+        "jsbeautifier==1.6.4",
         "colorama==0.3.7",
         "Pillow==3.2.0",
         "pythonaes==1.0",
-        "lxml==3.6.0",
+        "lxml==3.6.4",
     ],
     entry_points={
         "console_scripts": [
